d0688e3fb3f21616333ecb33fe56ebee3a691d71
Michael Mior <mmior@cs.rit.edu>
2019-07-12 20:47:41 -0400

    Add active beats and connecting lines

--- src/Wheel.js
+++ src/Wheel.js
@@ -6,6 +6,14 @@ const smallRadius = 2;
 const padding = 10;
 
 class Wheel extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      active: [],
+      ...props.initialState
+    };
+  }
+
   pointCoords(subdivision) {
     const angle = (Math.PI * 2) / this.props.subdivisions * subdivision;
     const x = this.props.radius / 2 - smallRadius + Math.sin(angle) * (this.props.radius + smallRadius) / 2 + padding;
@@ -15,18 +23,41 @@ class Wheel extends Component {
   }
 
   render() {
+    let lines = [];
+    let lineIndexes = this.state.active.slice();
+    lineIndexes.push(lineIndexes[0]);
+    for (let i = 1; i < lineIndexes.length; i++) {
+      const coords = this.pointCoords(lineIndexes[i]);
+      const prevCoords = this.pointCoords(lineIndexes[i - 1]);
+      lines.push(<line key={'line' + i} x1={coords.x} y1={coords.y} x2={prevCoords.x} y2={prevCoords.y} stroke='black' strokeWidth='2' />);
+    }
+
     return <div style={{position: 'relative', padding: padding + 'px'}}>
+      <svg style={{pointerEvents: 'none', position: 'absolute', top: padding / 2 + 'px', left: padding / 2 + 'px', zIndex: 100}} width={this.props.radius + smallRadius * 4} height={this.props.radius + smallRadius * 4}>
+        {lines}
+      </svg>
       <Circle borderColor='white' radius={this.props.radius + 'px'}>{Array.from({length: this.props.subdivisions}, (_, i) => {
       const coords = this.pointCoords(i);
-      return <div key={i} style={{
+      return <div key={'refCircle' + i} style={{
         cursor: 'pointer',
         position: 'absolute',
         top: coords.y + 'px',
         left: coords.x + 'px'
       }}>
-        <Circle borderColor='white' backgroundColor='white' radius={(smallRadius * 2)+ 'px'}/>
+        <Circle borderColor='white' backgroundColor='white' radius={(smallRadius * 2) + 'px'}/>
       </div>
       })}</Circle>
+      {this.state.active.map(i => {
+        const coords = this.pointCoords(i);
+        return <div key={'beatCircle' + i} style={{
+          position: 'absolute',
+          top: coords.y + 'px',
+          left: coords.x + 'px',
+          pointerEvents: 'none'
+        }}>
+          <Circle borderColor='black' backgroundColor='black' radius={(smallRadius * 2) + 'px'}/>
+        </div>
+      })}
     </div>;
   }
 }
--- src/stories/index.js
+++ src/stories/index.js
@@ -12,7 +12,7 @@ storiesOf('Circle', module).add('a simple circle', () => (
 ));
 
 storiesOf('Wheel', module).add('a simple wheel', () => (
-  <Wheel/>
+  <Wheel initialState={{active: [1, 8, 9, 13]}}/>
 ), {
   backgrounds: [{
     name: 'grey', value: 'rgba(150, 150, 150)',
