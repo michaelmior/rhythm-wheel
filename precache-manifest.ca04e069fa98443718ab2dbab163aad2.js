5ce929838763321cdce43634c66dc87702dff7e1
Michael Mior <mmior@cs.rit.edu>
2019-07-12 23:04:35 -0400

    Updates

---
+++ precache-manifest.ca04e069fa98443718ab2dbab163aad2.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "58029f53c750ae0d285da17e8a7e8ecb",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d86d5b7593343e2cb2fd",
+    "url": "/static/css/main.dd35c4ed.chunk.css"
+  },
+  {
+    "revision": "acc80d623c137348ac94",
+    "url": "/static/js/2.6e4a076c.chunk.js"
+  },
+  {
+    "revision": "d86d5b7593343e2cb2fd",
+    "url": "/static/js/main.2fd261bf.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
