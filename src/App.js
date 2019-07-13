7ef1c0d42434974348e033b4b9deef84ad05ccda
Michael Mior <mmior@cs.rit.edu>
2019-07-13 11:32:46 -0400

    Add GitHub corner

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
 import React from 'react';
 
+import GithubCorner from 'react-github-corner';
+
 import Player from './Player';
 
 function App() {
   return (
     <div className="App">
+      <GithubCorner href="https://github.com/michaelmior/rhythm-wheel" />
       <Player/>
     </div>
   );
