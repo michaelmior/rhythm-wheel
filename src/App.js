2b1e2ac36eb0ce665f1dba6a66e05e1d1221b6ce
Michael Mior <mmior@cs.rit.edu>
2019-07-14 13:55:35 -0400

    Add connection to Toussaint's work

--- src/App.js
+++ src/App.js
@@ -4,11 +4,14 @@ import GithubCorner from 'react-github-corner';
 
 import Player from './Player';
 
+import './App.css';
+
 function App() {
   return (
     <div className="App">
       <GithubCorner href="https://github.com/michaelmior/rhythm-wheel" />
       <Player/>
+      <footer>Inspired by <a href="https://www.amazon.com/Geometry-Musical-Rhythm-Makes-Second/dp/0815370970/">The Geometry of Musical Rhythm: What Makes a "Good" Rhythm Good?</a></footer>
     </div>
   );
 }
